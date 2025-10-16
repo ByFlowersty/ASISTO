49836d52a2569e2b13e54f3e6e07947ee9c2155a
ByFlowersty <a.flores.c141@edu.utc.mx>
2025-10-15 23:03:40 -0600

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,13 +7,11 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen text-gray-800 p-4 md:p-6 lg:p-8">
-        <div className="w-full mx-auto flex rounded-3xl shadow-2xl overflow-hidden bg-white/60 backdrop-blur-xl border border-white/20 h-[calc(100vh-2rem)] md:h-[calc(100vh-3rem)] lg:h-[calc(100vh-4rem)]">
-            <Sidebar />
-            <main className="flex-1 p-6 md:p-10 overflow-y-auto">
-                {children}
-            </main>
-        </div>
+    <div className="min-h-screen text-gray-800 bg-gray-50 flex">
+        <Sidebar />
+        <main className="flex-1 p-6 md:p-10 overflow-y-auto h-screen">
+            {children}
+        </main>
     </div>
   );
 };
