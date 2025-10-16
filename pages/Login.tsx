6cd7364183bb74883521bf384d69aee8f1f7fa95
ByFlowersty <a.flores.c141@edu.utc.mx>
2025-10-15 23:34:38 -0600

    Update Login.tsx

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -30,13 +30,11 @@ const Login: React.FC = () => {
         <div className="text-center">
           {/* Espacio para el logo */}
           <div className="mb-4">
-            <img
-              src="/logo.png" // <-- Cambia esto por la ruta de tu logo
+           <img
+              src="/nep.png"
               alt="Logo"
               className="mx-auto h-16 w-auto"
             />
-          </div>
-
           <div className="inline-flex items-center justify-center gap-3 mb-2">
             <svg className="h-10 w-10 text-primary-600" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M4 4h4v4H4V4z" fill="currentColor" />
